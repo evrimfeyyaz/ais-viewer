2678878a2a509824b66e6d137c9861d445b9bede
Evrim Persembe <evrimfeyyaz@gmail.com>
2025-05-06 19:56:38 +0300

    Add MapLibre React Native

--- frontend-mobile/App.tsx
+++ frontend-mobile/App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { MapView } from '@maplibre/maplibre-react-native';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView style={styles.map} />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  map: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
